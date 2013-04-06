a8799200bc2db6e72b0b9589827541024eb1b473
Srinivasan Vijayaraghavan <srinivav@andrew.cmu.edu>
2013-04-06 17:48:51 -0400

    Cleaned up server code. Works with /all

--- app.js
+++ app.js
@@ -12,26 +12,21 @@ app.use(express.bodyParser());
 
 var hostpath = "http://gentle-shore-9072.herokuapp.com/register";
 
-var reqCallback = function(error, response, body)  {
-  if (!error && response.statusCode == 200) {
-    var questions = JSON.parse(body);
-    questions.push({"success": "true"});
-    return questions;
-  }
-}
-
-// request(hostpath, reqCallback);
-
-  
 /* Just fetching all levels of questions for now
  * First, fetch the questions object from existing server. 
  * Then, add a success attribute to it
  * Finally, send back the appended object to the client
- */
+ *
+ * Note: Client sends a request to our server, which causes our server
+ * to send a request to their server. Two requests and responses going on
+ * at the same time here */
 app.get('/all', function (clientRequest, clientResponse) {
   request(hostpath, function(error, response, body) {
-    var allQuestions = reqCallback(error, response, body);
-    clientResponse.send(allQuestions);
+      if (!error && response.statusCode == 200) {
+        var questions = JSON.parse(body);
+        questions.push({"success": "true"});
+        clientResponse.send(questions);
+      }
   });
 });
 
@@ -46,5 +41,3 @@ app.get("/static/:staticFilename", function (request, response) {
 
 // Finally, activate the server at port 8889
  app.listen(8889);
-
-
