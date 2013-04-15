6f9124b279aff7bf58486b8c6adc2481535a6e6d
Srinivasan Vijayaraghavan <srinivav@andrew.cmu.edu>
2013-04-15 18:39:07 -0400

    Added more stuff to the canvas skeleton

--- static/game/canvas_skel.js
+++ static/game/canvas_skel.js
@@ -1,5 +1,51 @@
+// Skeleton for canvas
+// Srinivasan Vijayaraghavan (srinivav)
+
+/* TODO:
+ * touchStart, touchMove, touchEnd functions
+ * updateAllPositions function
+ */
+
 var canvas = $("#myCanvas");
 var ctx = canvas.getContext("2d");
+
+
+// Patch Bind -- from course notes
+// iOS5 or less does not have .bind so add it if needed (iOS6 has it!)
+function patchBind(){
+   if (Function.prototype.bind === undefined){
+      Function.prototype.bind = function (bind) {
+           var self = this;
+           return function () {
+               var args = Array.prototype.slice.call(arguments);
+               return self.apply(bind || null, args);
+           };
+       };
+   }
+}
+
+
+function onTouchStart(event)  {
+  event.preventDefault();
+  return;
+}
+
+function onTouchMove(event) {
+  event.preventDefault();
+  return;
+}
+
+function onTouchEnd(event)  {
+  event.preventDefault();
+  return;
+}
+
+
+function onTimer()  {
+  //updateAllPositions();
+  return;
+}
+
 
 
 // shim layer with setTimeout fallback
@@ -19,3 +65,13 @@ window.requestAnimFrame = (function(){
   render();
 })();
 
+
+function run()  {
+  canvas.addEventListener('touchstart', onTouchStart, false);
+  canvas.addEventListener('touchend', onTouchEnd, false);
+  canvas.addEventListener('touchmove', onTouchMove, false);
+  canvas.setAttribute('tabindex', '0');
+  canvas.focus();
+  intervalId = setInterval(onTimer, timerDelay);
+  animloop();
+}
