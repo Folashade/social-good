56cced89dd8395592cbaa5d09681fad603e4d7da
Folashade Okunubi <foo@andrew.cmu.edu>
2013-04-27 14:15:57 -0400

    beginning debug: adding circles

--- static/game/events.js
+++ static/game/events.js
@@ -17,6 +17,17 @@ function onMouseDown (event)  {
 }
 */
 
+function circle(ctx, cx, cy, radius) {
+    ctx.arc(cx, cy, radius, 0, 2*Math.PI, true);
+}
+ctx.beginPath();
+var cx = 200;
+var cy = 200;
+var radius = 100;
+circle(ctx, cx, cy, radius);
+ctx.fill();
+
+
 
 function inRadius (x, cx, y, cy, radius) {
   if ((Math.abs(x-cx) <= 4*qRadius) && (Math.abs(y-cy) <= radius))
@@ -49,6 +60,7 @@ function onMouseDown(event) {
       if (isQuestionBalloon(curBalloon) === true &&
           inRadius(x, cx, y, cy, qRadius) === true) {
         //Question balloon has been popped!
+		circle(ctx, cx, cy, qRadius)
         removeBalloon(i);
         i -= 1;
         wasQuestionPopped = true;
@@ -56,6 +68,7 @@ function onMouseDown(event) {
       }
 
       else if (inRadius(x, cx, y, cy, radius) === true) {
+		circle(ctx, cx, cy, radius)
         removeBalloon(i);
         i -= 1;
       }
