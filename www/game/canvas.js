40e6045619a75f94e991256f77f24ab58f405465
nsht13 <neshi13@gmail.com>
2013-04-29 19:58:34 -0400

    modified question UI

--- www/game/canvas.js
+++ www/game/canvas.js
@@ -48,11 +48,14 @@ var touches;
 // Spritesheet
 var spriteSheet = new Image();
 spriteSheet.src = "assets/sprite_sheet_canvas.png";
+var button=new Image();
+button.src="assets/button.png";
+var questions=[["where are _______ shows", "they", "their", "there", "they re"], ["where are _______ shows", "they", "their", "there", "they re"]];
 
 //from http://developer.mozilla.org/en-US/docs/Canvas_tutorial/Drawing_shapes
 function roundedRect(ctx,x,y,width,height,radius) {
     ctx.beginPath();
-    ctx.fillStyle = "rgba(0, 0, 0, 0.7)";
+    ctx.fillStyle = "rgba(0, 0, 0, 0.8)";
     ctx.moveTo(x,y+radius);
     ctx.lineTo(x,y+height-radius);
     ctx.quadraticCurveTo(x,y+height,x+radius,y+height);
@@ -62,8 +65,12 @@ function roundedRect(ctx,x,y,width,height,radius) {
     ctx.quadraticCurveTo(x+width,y,x+width-radius,y);
     ctx.lineTo(x+radius,y);
     ctx.quadraticCurveTo(x,y,x,y+radius);
-    ctx.stroke();
+ctx.closePath();
+    ctx.strokeStyle= "a77c50";
+    ctx.lineWidth=5;
+
     ctx.fill();
+        ctx.stroke();
 }
 
 
@@ -84,8 +91,27 @@ function drawQuestionScreen() {
   ctx.font = "bold 14px sans-serif";
   ctx.textAlign = "right";
   ctx.textBaseline = "middle";
-  ctx.strokeStyle = "aqua";
-  ctx.strokeText(text, 675, 120);
+  ctx.fillStyle="14e2e2";
+  ctx.lineWidth=1;
+  ctx.font='5em Rumpelstiltskin';
+  ctx.fillText(text, 675, 150);
+  ctx.fillStyle="a77c50";
+  ctx.lineWidth=1;
+  ctx.font='2em Maven Pro';
+  var randomquestion= Math.floor(Math.random());
+  console.log(randomquestion);
+  var option1= questions[randomquestion][1];
+  var option2= questions[randomquestion][2];
+  var option3= questions[randomquestion][3];
+  var option4= questions[randomquestion][4];
+  ctx.drawImage(button, 80, 200);
+  ctx.fillText(option1, 150, 250);
+  ctx.drawImage(button, 410, 200);
+  ctx.fillText(option2, 500, 250);
+  ctx.drawImage(button, 80, 300);
+  ctx.fillText(option3, 150, 350);
+  ctx.drawImage(button, 410, 300);
+  ctx.fillText(option4, 500, 350);
 }
 
 function drawPoints() {
@@ -93,8 +119,10 @@ function drawPoints() {
   ctx.font = "vold 14px sans-serif";
   ctx.textAlign = "left";
   ctx.textBaseline = "middle";
-  ctx.strokeStyle = "aqua";
-  ctx.strokeText(text, 10, 10);
+  ctx.font='4em Rumpelstiltskin';
+  ctx.strokeStyle = "black";
+
+  ctx.fillText(text, 20, 20);
 }
 
 function render()  {
